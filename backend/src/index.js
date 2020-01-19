76f8f03ec5fbea147e4f0bd09744a9f4724b4ed1
William Koller <wkoller25@gmail.com>
2020-01-19 15:55:39 -0300

    adding setupWebscoket

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,52 +1,31 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const http = require('http');
+
 const routes = require('./routes');
+const { setupWebsocket } = require('./websocket');
+
 
 const app = express();
+const server = http.Server(app);
+
+setupWebsocket(server);
+
 
 mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-1cbkb.mongodb.net/week10?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
-
 });
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-// Métodos HTTP: GET, POST PUT E DELETE
-
-// Tipos de parâmetros:
-
-// Query Params: request.query(Filtros, ordenação, paginação...)
-
-app.get('/', (request, response) => {
-  console.log(request.query);
-  return response.json({ message: "Hello GET used request.query"})
-});
+server.listen(3332);
 
 
-// Route Params: request.params(Identifica um recurso na alteração ou remoção)
-
-app.put('/users/:id', (request, response) => {
-  console.log(request.query)
-  return response.json({ message: 'Hello OmniStack 11' });
-});
-
-// Body: request.body(Dados para criação ou alteração de um registro)
-
-app.post('/users', (request, response) => {
-  console.log(request.body);
-  return response.json({ message: "Hello POST used request.body"})
-});
-
-// http://localhost:3333
-
-app.listen(3333);
-
-// MongoDB (Não-Relacional)
 
 
 
